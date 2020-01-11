ea8f80284030d3b5eea1c5e2a74d3da03b73f934
Mishan Madhupa <mishancool2016@gmail.com>
2020-01-11 19:25:26 +0530

    Read file and print data line by line

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Seat-reservation-system-for-flights
 Seat reservation system for flights using OOP concept
+
+# Compail c++ code
+g++ .\flight2.cpp -o fli.exe
+
+# Run c++ code
+.\fli.exe
--- flight2.cpp
+++ flight2.cpp
@@ -6,16 +6,7 @@ int main(){
 
    fstream file; 
    string filename; 
- //  newfile.open("Flights.txt",ios::out);  // open a file to perform write operation using file object
-//    if(newfile.is_open()) //checking whether the file is open
-//    {
-//       newfile<<"Tutorials point \n";   //inserting text
-//       newfile.close();    //close the file object
-//    }
-//    newfile.open("tpoint.txt",ios::in); //open a file to perform read operation using file object
-
-    // filename of the file 
-    filename = "Flights.txt"; 
+   filename = "Flights.txt"; 
   
     // opening file 
     file.open(filename.c_str()); 
