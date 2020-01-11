92b3458ef5f24b52d7fe4ddeb6aef60ba5131b37
Mishan Madhupa <mishancool2016@gmail.com>
2020-01-11 20:17:31 +0530

    Get data to statement array

--- flight3.cpp
+++ flight3.cpp
@@ -5,19 +5,33 @@ using namespace std;
 int main()
 {
     ifstream inFile;
+    string statement[100];
     // open the file stream
     inFile.open(".\\Flights.txt");
+
     // check if opening a file failed
     if (inFile.fail()) {
         cerr << "Error opeing a file" << endl;
         inFile.close();
         exit(1);
     }
+
+    int count1=0;
     string line;
     while (getline(inFile, line))
     {
-        cout << line << endl;
+        // cout << line << endl;
+        statement[count1]=line;
+        count1++;
+
     }
+
+    cout << "count1 is :" << count1 << endl;
+
+    for(int i = 0 ; i<=count1 ; i++){
+        cout << "value :" << statement[i] << endl;
+    }
+
     // close the file stream
     inFile.close();
 }
